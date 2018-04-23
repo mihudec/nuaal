301ad6b6911ee340c7b774338128c9211ff8ef73
mijujda <mijujda@gmail.com>
2018-04-24 01:25:27 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup
 setup(
     name='nuaal',
     version='0.1.0',
-    packages=['nuaal', 'nuaal.tests', 'nuaal.utils', 'nuaal.Parsers', 'nuaal.Readers', 'nuaal.Writers', 'nuaal.examples', 'nuaal.Discovery',
-              'nuaal.connections', 'nuaal.connections.api', 'nuaal.connections.api.epnm', 'nuaal.connections.api.apic-em', 'nuaal.connections.cli',
-              'nuaal.connections.snmp'],
+    packages=['nuaal', 'nuaal.utils', 'nuaal.Parsers', 'nuaal.Readers', 'nuaal.Writers', 'nuaal.examples', 'nuaal.Discovery',
+              'nuaal.connections.cli'],
     url='https://github.com/mijujda/nuaal',
     license='',
     author='Miroslav Hudec',
