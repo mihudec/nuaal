eb787cf187006069e63d1d77c14a52b95cb95e73
mijujda <mijujda@gmail.com>
2020-11-24 15:19:43 +0100

    Fix directory name

--- nuaal/discovery/__init__.py
+++ nuaal/discovery/__init__.py
@@ -1,3 +1,3 @@
-from nuaal.discovery.Neighbor_Discovery import Neighbor_Discovery
+from nuaal.discovery.Neighbor_Discovery_temp import Neighbor_Discovery
 from nuaal.discovery.IP_Discovery import IP_Discovery
 from nuaal.discovery.Topology import CliTopology, Topology
