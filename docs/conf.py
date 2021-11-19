3445f986cabdfd7a9d43259bdbdc989d99731c72
Miroslav Hudec <mijujda@gmail.com>
2021-11-19 12:59:34 +0100

    Fix regex in show interfaces

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ author = 'nuaal'
 add_module_names = False
 
 # The short X.Y version
-version = '0.1.22'
+version = '0.1.23'
 # The full version, including alpha/beta/rc tags
-release = '0.1.22-beta'
+release = '0.1.23-beta'
 
 
 # -- General configuration ---------------------------------------------------
